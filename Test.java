3a2053b128562aa2b3459e844d9e2a82d7a6b94a
Geoffrey Parker <professional.cynic@gmail.com>
2010-11-17 20:31:23 -0600

    Removed html files from git tracking

--- Test.java
+++ Test.java
@@ -11,25 +11,25 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		ADisk adisk = new ADisk(true);
-		TransID tid2 = adisk.beginTransaction();
-		TransID tid1 = adisk.beginTransaction();
-		byte[] wbuffer = ADisk.blankSector();
-		byte[] rbuffer = ADisk.blankSector();
-		
-		int sectorNum = 1025;
-		
-		ADisk.fill(wbuffer, "Hello World".getBytes());
-		
-		adisk.writeSector(tid1,sectorNum, wbuffer);
-		try {
-			adisk.readSector(tid1, sectorNum, rbuffer);
-			adisk.commitTransaction(tid1);
-			adisk.readSector(tid2, sectorNum, rbuffer);
-			Thread.sleep(2000);
-			adisk.readSector(tid2, sectorNum, rbuffer);
-		}catch (Exception e) {
-			assert(false);
-		}
+//		TransID tid2 = adisk.beginTransaction();
+//		TransID tid1 = adisk.beginTransaction();
+//		byte[] wbuffer = ADisk.blankSector();
+//		byte[] rbuffer = ADisk.blankSector();
+//		
+//		int sectorNum = 1025;
+//		
+//		ADisk.fill(wbuffer, "Hello World".getBytes());
+//		
+//		adisk.writeSector(tid1,sectorNum, wbuffer);
+//		try {
+//			adisk.readSector(tid1, sectorNum, rbuffer);
+//			adisk.commitTransaction(tid1);
+//			adisk.readSector(tid2, sectorNum, rbuffer);
+//			Thread.sleep(2000);
+//			adisk.readSector(tid2, sectorNum, rbuffer);
+//		}catch (Exception e) {
+//			assert(false);
+//		}
 		System.out.println("Done.");
 		System.exit(0);
 	}
